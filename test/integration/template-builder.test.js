a5f4668b2775cffd575022ccbc5eb30ae487fce2
fabifrank <info@fabifrank.com>
2017-11-26 15:45:34 +0100

    two integration tests working

--- test/integration/template-builder.test.js
+++ test/integration/template-builder.test.js
@@ -4,14 +4,13 @@ var fs = require('fs')
 var builder = require('../../src/template-builder')
 var helper = require('./_helper')
 
-//test.after(helper.clean)
+test.beforeEach(() => helper.createTestFiles())
+
+test.afterEach(() => helper.clean())
 
 test.cb('#buildOut builds files correctly', t => {
-  helper.createTestFiles();
-
   var fn = builder.buildOut(helper.CONFIG_CONTENT_JSON, helper.TEST_FILE_1_CONTENT, helper.TEST_FILE_1_PATH, 'production');
   fn(() => {
-    console.log('test1')
     var content = fs.readFileSync(helper.TEST_FILE_1_PATH_BUILT_PRODUCTION, 'utf8')
     t.is(content, `
   Hello production.
@@ -22,11 +21,8 @@ test.cb('#buildOut builds files correctly', t => {
 });
 
 test.cb('#wrapFile does correctly buildout different envs', t => {
-  helper.createTestFiles();
-
   var fn = builder.wrapFile(helper.TEST_FILE_1_PATH, helper.CONFIG_CONTENT_JSON);
   fn(() => {
-    console.log('test2')
     var contentDev = fs.readFileSync(helper.TEST_FILE_1_PATH_BUILT_DEV, 'utf8')
     t.is(contentDev, `
   Hello dev.
